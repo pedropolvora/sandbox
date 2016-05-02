e3d8db5bfcd8f4b10864d35b4d94fa67793b6fb4
Pedro Polvora <pedro.polvora@gmail.com>
2016-05-03 00:30:28 +0200

    first version of react1st done

--- react_first_app/js/app.js
+++ react_first_app/js/app.js
@@ -3,11 +3,19 @@
   var item_data = [
       {
           id:1,
-          text:'item1'
+          text:'Load all the items and not just the first one.'
       },
       {
           id:2,
-          text:'item 2'
+          text:'Do it with a button'
+      },
+      {
+          id:3,
+          text:'Add a button to remove items'
+      },
+      {
+          id:4,
+          text:'Say when you reached the end of the file'
       }
   ];
   var ToDoList = React.createClass({
